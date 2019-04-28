79565b8b55fd5d6715172cbecf2ddcbbbb9ac82c
Matthew de Verteuil <onceuponajooks@gmail.com>
2019-04-27 22:10:54 -0400

    Resolve bugs in user account

--- mig3/accounts/models.py
+++ mig3/accounts/models.py
@@ -1,10 +1,43 @@
 import secrets
 
 from django.conf import settings
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 from hashid_field import HashidAutoField
 from model_utils.models import TimeStampedModel
+
+
+class UserAccountManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """Create and save a user with the given email and password."""
+        if not email:
+            raise ValueError("Email is a required field.")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
+    def get_by_natural_key(self, email):
+        return super().get_by_natural_key(email)
 
 
 class UserAccount(TimeStampedModel, AbstractBaseUser, PermissionsMixin):
@@ -28,7 +61,7 @@ class UserAccount(TimeStampedModel, AbstractBaseUser, PermissionsMixin):
         ),
     )
 
-    objects = UserManager()
+    objects = UserAccountManager()
 
     EMAIL_FIELD = USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
