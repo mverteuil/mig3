d37a183cd9bf3e10aa2fcba7483d26b2d7ebf632
Matthew de Verteuil <onceuponajooks@gmail.com>
2019-05-05 20:02:06 -0400

    Use vue routing for all non explicit django urls

--- mig3/mig3/urls.py
+++ mig3/mig3/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="application.html")),
     path("admin/", admin.site.urls),
     path("api/", include(("api.urls", "api"))),
+    re_path(".*", TemplateView.as_view(template_name="application.html")),
 ]
