6bb59e417606e75d8f1aa9e3fecc1a855b3d45fe
Matthew de Verteuil <onceuponajooks@gmail.com>
2019-05-05 22:53:44 -0400

    Added AppNavigation

--- mig3-ui/src/App.vue
+++ mig3-ui/src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <v-app>
+    <span>something here</span>
+    <app-navigation></app-navigation>
     <v-content transition="slide-x-transition">
       <router-view></router-view>
     </v-content>
@@ -7,8 +9,11 @@
 </template>
 
 <script>
+import AppNavigation from "@/components/AppNavigation";
+
 export default {
-  name: "App"
+  name: "App",
+  components: { AppNavigation }
 };
 </script>
 
---
+++ mig3-ui/src/components/AppNavigation.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-toolbar color="red darken-4" app dark>
+    <v-toolbar-title>MIG3</v-toolbar-title>
+    <template v-slot:extension>
+      <v-tabs color="transparent" icons-and-text>
+        <v-tab to="/">
+          Projects
+          <v-icon>phone</v-icon>
+        </v-tab>
+        <v-tab href="admin/">
+          Admin
+          <v-icon>phone</v-icon>
+        </v-tab>
+      </v-tabs>
+    </template>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: "AppNavigation"
+};
+</script>
+
+<style scoped></style>
