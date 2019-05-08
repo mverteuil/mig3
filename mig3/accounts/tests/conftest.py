bac7b18eceef1ca08f9abb37e915e3402d0da394
Matthew de Verteuil <onceuponajooks@gmail.com>
2019-05-08 15:56:55 -0400

    CQIs

--- mig3/accounts/tests/conftest.py
+++ mig3/accounts/tests/conftest.py
@@ -2,24 +2,31 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 
 import pytest
+from rest_framework.test import APIClient
 
 from accounts import models as accounts
 
 
 @pytest.fixture
+def api_client():
+    """Create an unauthenticated API test client."""
+    return APIClient()
+
+
+@pytest.fixture
 def builder_account(db) -> accounts.BuilderAccount:
-    """Create a BuilderAccount fixture."""
+    """Create a BuilderAccount."""
     return accounts.BuilderAccount.objects.create(name="Test CI Service")
 
 
 @pytest.fixture
 def user_account(db) -> settings.AUTH_USER_MODEL:
-    """Create a UserAccount fixture."""
-    return get_user_model().objects.create(email="user@example.com", name="Test User", password="password")
+    """Create a UserAccount."""
+    return get_user_model().objects.create_user(name="Test User", email="user@example.com", password="password")
 
 
 @pytest.fixture
-def session_authentication(client, user_account):
+def session_authentication(api_client, user_account):
     """Create a test client and UserAccount authenticated by session."""
-    client.login(username=user_account.email, password="password")
-    return (client, user_account)
+    api_client.login(username=user_account.email, password="password")
+    return (api_client, user_account)
--- mig3/accounts/tests/test_views.py
+++ mig3/accounts/tests/test_views.py
@@ -1,3 +1,21 @@
-def test_user_account_list(session_authentication):
+from django.urls import reverse
+
+import pytest
+from rest_framework import status
+
+
+@pytest.mark.parametrize("view_name", ["api:user_account_list", "api:builder_account_list"])
+def test_view_with_session(session_authentication, view_name):
     """Should return serialized result."""
-    pass
+    client, user_account = session_authentication
+    url = reverse(view_name)
+    response = client.get(url)
+    assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.parametrize("view_name", ["api:user_account_list", "api:builder_account_list"])
+def test_view_without_session(client, view_name):
+    """Should refuse unauthenticated request."""
+    url = reverse(view_name)
+    response = client.get(url)
+    assert response.status_code == status.HTTP_403_FORBIDDEN
