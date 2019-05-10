869310c34d2f3d4194c89d505dec454f9838fe02
Matthew de Verteuil <onceuponajooks@gmail.com>
2019-05-10 00:26:10 -0400

    Added builder statistics to serializers

--- mig3/accounts/api/serializers.py
+++ mig3/accounts/api/serializers.py
@@ -4,17 +4,26 @@ from django.contrib.auth import get_user_model
 from hashid_field import rest as hashid_field
 from rest_framework import serializers
 
+from api.serializers import ReadOnlySerializer
 from .. import models as accounts
+
+
+class BuilderStatisticsSerializer(ReadOnlySerializer):
+    """Summary API representation for builder relationship counts."""
+
+    build_count = serializers.IntegerField()
+    version_count = serializers.IntegerField()
 
 
 class BuilderAccountSerializer(serializers.ModelSerializer):
     """API representation of a CI/build service."""
 
     id = hashid_field.HashidSerializerCharField(source_field="accounts.BuilderAccount.id")
+    statistics = BuilderStatisticsSerializer()
 
     class Meta:  # noqa: D106
         model = accounts.BuilderAccount
-        fields = ("id", "name")
+        fields = ("id", "name", "statistics")
 
 
 class UserAccountField(serializers.Field):
