84e8652315818dce8a5712e0cd2b4fba3dcaea8d
Matthew de Verteuil <onceuponajooks@gmail.com>
2019-05-09 22:38:09 -0400

    Use real API result for mock data in project list

--- mig3-ui/src/sections/ProjectList.vue
+++ mig3-ui/src/sections/ProjectList.vue
@@ -18,15 +18,15 @@
             <v-card-actions class="space-evenly">
               <div>
                 <v-icon class="vertical-middle">mdi-bullseye-arrow</v-icon>
-                <span class="icon-text">2</span>
+                <span class="icon-text">{{ project.statistics.target_count }}</span>
               </div>
               <div>
                 <v-icon class="vertical-middle">mdi-folder</v-icon>
-                <span class="icon-text">10</span>
+                <span class="icon-text">{{ project.statistics.module_count }}</span>
               </div>
               <div>
                 <v-icon class="vertical-middle">mdi-test-tube</v-icon>
-                <span class="icon-text">60</span>
+                <span class="icon-text">{{ project.statistics.test_count }}</span>
               </div>
             </v-card-actions>
           </v-card>
@@ -43,19 +43,14 @@ export default {
     projects: [
       {
         id: "qL70nKe",
-        name: "Barbara",
-        repository_url: "https://github.com/mverteuil/barbara",
-        to: "projects/0/"
-      },
-      {
-        id: "E0lrPbR",
-        name: "mig3-client",
-        repository_url: "https://github.com/mverteuil/mig3-client"
-      },
-      {
-        id: "vJ7v5lA",
         name: "mig3",
-        repository_url: "https://github.com/mverteuil/mig3"
+        url: "http://localhost:8000/api/projects/qL70nKe/",
+        repo_url: "https://github.com/mverteuil/mig3.git",
+        statistics: {
+          target_count: 1,
+          module_count: 3,
+          test_count: 20
+        }
       }
     ]
   })
