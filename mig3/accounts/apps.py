ba3aa24ad21a9251735c1e4d867f122b3044d1b2
Matthew de Verteuil <onceuponajooks@gmail.com>
2019-05-14 19:59:47 -0400

    Added user account log message

--- mig3/accounts/apps.py
+++ mig3/accounts/apps.py
@@ -1,7 +1,19 @@
+import logging
+
 from django.apps import AppConfig
+
+logger = logging.getLogger(__name__)
 
 
 class AccountsConfig(AppConfig):
     """User and API consumer account and authentication details."""
 
     name = "accounts"
+
+    def ready(self):
+        """Perform checks on application load."""
+        user_model = self.get_model("UserAccount", True)
+        if not user_model.objects.count():
+            logger.info(
+                "You have not created an administrator account yet. Please visit this URL and create the administrator account: %URL%"
+            )
