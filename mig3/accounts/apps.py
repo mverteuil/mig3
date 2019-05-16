cd8e57ffc2873e53da96e8d23e8a76633e11792c
Matthew de Verteuil <onceuponajooks@gmail.com>
2019-05-15 21:43:40 -0400

    Handle pre-migration state in check

--- mig3/accounts/apps.py
+++ mig3/accounts/apps.py
@@ -3,6 +3,7 @@ import logging
 from django.apps import AppConfig, apps
 from django.conf import settings
 from django.core.checks import Warning, register
+from django.db import ProgrammingError
 from django.urls import reverse
 
 from accounts.utils import URLSignature
@@ -21,14 +22,17 @@ def check_for_missing_administrator_account(app_configs, **kwargs):
     """Check for an existing administrator account and provide a bootstrapping URL if missing."""
     errors = []
     user_model = apps.get_model("accounts", "UserAccount", require_ready=True)
-    if not user_model.objects.filter(is_superuser=True).count() > 0:
-        host = "http://localhost:8000" if settings.DEBUG else "https://your-site.heroku.com"
-        create_admin_url = reverse("create_admin", kwargs={"secret": URLSignature.generate_signature()})
-        errors.append(
-            Warning(
-                f"Please visit this URL and create your administrator account: {host}{create_admin_url}",
-                id="accounts.W001",
-                obj=user_model,
+    try:
+        if not user_model.objects.filter(is_superuser=True).count() > 0:
+            host = "http://localhost:8000" if settings.DEBUG else "https://your-site.heroku.com"
+            create_admin_url = reverse("create_admin", kwargs={"secret": URLSignature.generate_signature()})
+            errors.append(
+                Warning(
+                    f"Please visit this URL and create your administrator account: {host}{create_admin_url}",
+                    id="accounts.W001",
+                    obj=user_model,
+                )
             )
-        )
+    except ProgrammingError:
+        pass  # Migrations have not run yet.
     return errors
