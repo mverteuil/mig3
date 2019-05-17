b1dacde1fd39a04ef599ea14310e2887ac9cfb55
Matthew de Verteuil <onceuponajooks@gmail.com>
2019-05-17 17:13:35 -0400

    Stick to staticmethods and classmethods

--- mig3/wizard/models.py
+++ mig3/wizard/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from django.db import models
-from django.utils.decorators import classproperty
 
 from accounts import models as accounts
 from builds import models as builds
@@ -12,31 +11,37 @@ class SetupProgressStagesMeta:
 
     def __new__(cls, name, bases, dct):
         """Build class with STAGES defined."""
-        new_class = type(name, bases, dct)
-        new_class.STAGES = [stage_name for stage_name, property in dct.items() if isinstance(property, classproperty)]
-        return new_class
+        dct["STAGES"] = [
+            name
+            for name, prop in dct.items()
+            if name.startswith("has_") and isinstance(prop, (classmethod, staticmethod))
+        ]
+        return type(name, bases, dct)
 
 
 class SetupProgress(metaclass=SetupProgressStagesMeta):
     """Determine how much progress has been made in setting up the mig3 configuration."""
 
-    @classproperty
-    def has_administrator(self) -> bool:
+    #: Declared progress stages, populated by SetupProgressStagesMeta
+    STAGES: list = []
+
+    @staticmethod
+    def has_administrator() -> bool:
         """Has at least one active administrator."""
         return get_user_model().objects.filter(is_active=True, is_superuser=True).exists()
 
-    @classproperty
-    def has_builder(self) -> bool:
+    @staticmethod
+    def has_builder() -> bool:
         """Has at least one Builder Account."""
         return accounts.BuilderAccount.objects.exists()
 
-    @classproperty
-    def has_project(self) -> bool:
+    @staticmethod
+    def has_project() -> bool:
         """Has at least one project."""
         return projects.Project.objects.exists()
 
-    @classproperty
-    def has_targets(self) -> bool:
+    @staticmethod
+    def has_targets() -> bool:
         """Has a project with at least two targets.
 
         Two or more targets are expected to be present:
@@ -47,21 +52,21 @@ class SetupProgress(metaclass=SetupProgressStagesMeta):
         projects_with_target_counts = projects.Project.objects.annotate(target_count=models.Count("target"))
         return projects_with_target_counts.filter(target_count__gte=2).exists()
 
-    @classproperty
-    def has_builds(self) -> bool:
+    @staticmethod
+    def has_builds() -> bool:
         """Has a build for each target."""
         return builds.Build.objects.distinct("target").count() >= 2
 
-    @classproperty
-    def has_working_installation(self) -> bool:
+    @classmethod
+    def has_working_installation(cls) -> bool:
         """All setup progress stages are complete."""
-        return all(getattr(self, stage) for stage in self.STAGES if stage != "has_working_installation")
+        return all(getattr(cls, stage)() for stage in cls.STAGES if stage != "has_working_installation")
 
     @classmethod
     def get_current_stage_index(cls) -> int:
         """Get the index for the current setup step."""
         for index, stage in enumerate(cls.STAGES):
-            if not getattr(cls, stage):
+            if not getattr(cls, stage)():
                 return index
         else:
             return len(cls.STAGES) - 1
--- mig3/wizard/tests.py
+++ mig3/wizard/tests.py
@@ -1,7 +1,5 @@
 from unittest import mock
 
-from django.utils.decorators import classproperty
-
 from model_mommy import mommy
 
 from . import models as wizard
@@ -11,68 +9,71 @@ def test_stages_metaclass():
     """Should attach STAGES list to class, with stages in the order they are defined."""
     # noqa: D202
     class ClassWithStages(metaclass=wizard.SetupProgressStagesMeta):
-        @classproperty
-        def a(self):
-            pass
-
-        @classproperty
-        def b(self):
-            pass
-
-        def c(self):
+        @staticmethod
+        def has_static():
             pass
 
         @classmethod
-        def d(cls):
+        def has_klass(cls):
+            pass
+
+        def has_instance(self):
             pass
 
         @staticmethod
-        def e():
+        def static():
             pass
 
-    assert ClassWithStages.STAGES == ["a", "b"]
+        @classmethod
+        def klass(cls):
+            pass
+
+        def instance(self):
+            pass
+
+    assert ClassWithStages.STAGES == ["has_static", "has_klass"]
 
 
 def test_no_progress(db):
     """All stages should be negative at the beginning."""
     for stage in wizard.SetupProgress.STAGES:
-        assert getattr(wizard.SetupProgress, stage) is False
+        assert getattr(wizard.SetupProgress, stage)() is False
 
 
 def test_has_administrator(admin_user):
     """Should detect administrator and next stage."""
-    assert wizard.SetupProgress.has_administrator
+    assert wizard.SetupProgress.has_administrator()
     assert wizard.SetupProgress.get_current_stage_index() == wizard.SetupProgress.STAGES.index("has_administrator") + 1
 
 
 def test_has_builder(admin_user, builder_account):
     """Should detect builder and next stage."""
-    assert wizard.SetupProgress.has_builder
+    assert wizard.SetupProgress.has_builder()
     assert wizard.SetupProgress.get_current_stage_index() == wizard.SetupProgress.STAGES.index("has_builder") + 1
 
 
 def test_has_project(admin_user, builder_account, project):
     """Should detect builder and next stage."""
-    assert wizard.SetupProgress.has_builder
+    assert wizard.SetupProgress.has_builder()
     assert wizard.SetupProgress.get_current_stage_index() == wizard.SetupProgress.STAGES.index("has_project") + 1
 
 
 def test_has_targets_without_enough(admin_user, builder_account, primary_target, project):
     """Should require two project targets."""
-    assert not wizard.SetupProgress.has_targets
+    assert not wizard.SetupProgress.has_targets()
     assert wizard.SetupProgress.get_current_stage_index() == wizard.SetupProgress.STAGES.index("has_targets")
 
 
 def test_has_targets_without_enough_for_same_project(admin_user, builder_account, primary_target, project):
     """Should require two targets attached to the same project."""
     mommy.make("projects.Target")
-    assert not wizard.SetupProgress.has_targets
+    assert not wizard.SetupProgress.has_targets()
     assert wizard.SetupProgress.get_current_stage_index() == wizard.SetupProgress.STAGES.index("has_targets")
 
 
 def test_has_targets(admin_user, builder_account, primary_target, project, secondary_target):
     """Should detect two targets and next stage."""
-    assert wizard.SetupProgress.has_targets
+    assert wizard.SetupProgress.has_targets()
     assert wizard.SetupProgress.get_current_stage_index() == wizard.SetupProgress.STAGES.index("has_targets") + 1
 
 
@@ -80,7 +81,7 @@ def test_has_builds_without_enough(
     admin_user, primary_build, builder_account, project, primary_target, secondary_target
 ):
     """Should require at least two builds."""
-    assert not wizard.SetupProgress.has_builds
+    assert not wizard.SetupProgress.has_builds()
     assert wizard.SetupProgress.get_current_stage_index() == wizard.SetupProgress.STAGES.index("has_builds")
 
 
@@ -89,25 +90,30 @@ def test_has_builds_without_enough_targets(
 ):
     """Should require at least two builds from different targets."""
     mommy.make("builds.Build", target=primary_target)
-    assert not wizard.SetupProgress.has_builds
+    assert not wizard.SetupProgress.has_builds()
     assert wizard.SetupProgress.get_current_stage_index() == wizard.SetupProgress.STAGES.index("has_builds")
 
 
 def test_has_builds(admin_user, builder_account, primary_build, primary_target, project, secondary_target):
     """Should require at least two builds from different targets."""
     mommy.make("builds.Build", target=secondary_target)
-    assert wizard.SetupProgress.has_builds
+    assert wizard.SetupProgress.has_builds()
     assert wizard.SetupProgress.get_current_stage_index() == wizard.SetupProgress.STAGES.index(
         "has_working_installation"
     )
 
 
 @mock.patch.multiple(
-    wizard.SetupProgress, has_administrator=True, has_builds=True, has_builder=True, has_project=True, has_targets=True
+    wizard.SetupProgress,
+    has_administrator=lambda: True,
+    has_builds=lambda: True,
+    has_builder=lambda: True,
+    has_project=lambda: True,
+    has_targets=lambda: True,
 )
 def test_has_working_installation():
     """Should require at least two builds from different targets."""
-    assert wizard.SetupProgress.has_working_installation
+    assert wizard.SetupProgress.has_working_installation()
     assert wizard.SetupProgress.get_current_stage_index() == wizard.SetupProgress.STAGES.index(
         "has_working_installation"
     )
