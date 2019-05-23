0d3249b123770e417f75f41381cef3bf3906e242
Matthew de Verteuil <onceuponajooks@gmail.com>
2019-05-23 01:30:01 -0400

    Essentially working setup wizard, needs polish

--- mig3-ui/src/components/installationsetup/InstallationSetupBuilds.vue
+++ mig3-ui/src/components/installationsetup/InstallationSetupBuilds.vue
@@ -1,24 +1,42 @@
 <template lang="pug">
   v-stepper-content(:step="step")
-    v-container(fluid)
+    v-container(fluid ma-0 pa-0)
       v-layout
         v-flex(xs12)
-          v-subheader It's time to send up a build for each target to make sure everything is configured correctly.
-      v-layout(row)
-        v-flex(xs4)
-          v-container
-            v-layout(column)
-              v-flex To install the client <kbd>pip install mig3-client</kbd>, add this to your development/test requirements.txt. If you use pipenv.
-              v-flex Add a <code>setup.cfg</code> if you don't already have one with a py.test section to generate the report. If you use <code>pytest.ini</code>, you should add it there instead.
-                code
-                  | [tool:pytest]
-                  | json_report = .report.json
-              v-flex Run py.test for the first configuration and generate the JSON report. It will now exist as .report.json. (TIP: Add this file to your <code>.gitignore</code> to prevent accidentally committing it.)
-              v-flex To submit a report to the mig3 service, we'll need to tell the service which project and target we've tested and the builder's token will authenticate the submission.
-                | (TIP: Remember that the builder token is a <b>secret</b> and should not be displayed in plaintext to anyone but your administrators.)
-              v-flex(v-for="target in targets" :key="target.name")
-                v-subheader Target: {{ target.name }}
-                kbd mig3 --target {{ target.id }} --build 0 --endpoint {{ locationProtocol }}//{{ locationHostname }}/api/builds/ --token {{ builder.token }}
+          v-subheader(class="pl-0") It's time to send up a build for each target to make sure everything is configured correctly.
+      v-layout(column)
+        v-flex(xs12)
+          v-card
+            v-card-title 1. Install the client
+            v-card-text
+              p <kbd>pip install mig3-client</kbd>
+              p Add this to your development/test <code>requirements.txt</code>.
+              p If you use pipenv, add it to your Pipfile's dev section:
+              p <kbd>pipenv install --dev mig3-client</kbd>
+          v-card
+            v-card-title 2. Add py.test JSON report configuration
+            v-card-text
+              p Add a <code>setup.cfg</code> if you don't already have one with a py.test section to generate the report. However, if you already use <code>pytest.ini</code>, you should add it there instead.
+              kbd(pl-0) [tool:pytest]<br>
+                | json_report = .report.json
+        v-card
+          v-card-title 3. Run py.test and generate a report
+          v-card-text
+            p Run <code>py.test</code> for the first configuration and generate the JSON report. It will now exist as <code>.report.json</code>.
+          v-card-actions TIP Add this <code>.report.json</code> to your <code>.gitignore</code> to prevent accidentally committing it.
+        v-card
+          v-card-title 4. Submit your report to the mig3 service
+          v-card-text
+            p To submit a report to the mig3 service, we'll need to tell the service which project and target we've tested and the builder's token will authenticate the submission.
+            p(v-for="target in targets" :key="target.name")
+              v-subheader Target: {{ target.name }} <v-spacer/><v-progress-circular v-if="target.builds.length === 0" indeterminate/>
+              kbd mig3 --target {{ target.id }} \<br>
+                |       --build 0 \
+                |       --endpoint {{ locationProtocol }}//{{ locationHostname }}/api/builds/ \
+                |       --token {{ builder.token }}
+          v-card-actions (TIP: Remember that the builder token is a <strong>secret</strong> and should not be displayed in plaintext to anyone but your administrators.)
+
+
 </template>
 <script>
 import { mapState } from "vuex";
@@ -37,11 +55,29 @@ export default {
   },
   data: () => ({
     polling: null,
-    builder: { id: null, name: null, token: null },
+    builder: {
+      id: null,
+      name: null,
+      token: null
+    },
     targets: {}
   }),
   props: ["step"],
-  mounted() {
+  methods: {
+    updateTargetDetails() {
+      Object.values(this.targets).forEach(({ id }) => {
+        apiClient.getTargetDetails(id).then(response => {
+          this.$set(this.targets, response.data.id, response.data);
+        });
+      });
+    }
+  },
+  watch: {
+    targets: newValue => {
+      this.targetValues = Object.values(newValue);
+    }
+  },
+  created() {
     apiClient.getBuilders().then(response => {
       apiClient.getBuilderDetails(response.data[0].id).then(response => {
         this.builder = response.data;
@@ -49,17 +85,21 @@ export default {
     });
     apiClient.getProjects().then(response => {
       apiClient.getProjectDetails(response.data[0].id).then(response => {
-        this.targets = response.data.targets.map(target => [target.id, target]);
-      });
-    });
-    this.polling = setInterval(() => {
-      this.targets.forEach(([id]) => {
-        apiClient.getTargetDetails(id).then(response => {
-          this.targets[response.data.id] = response.data;
+        response.data.targets.forEach(target => {
+          this.$set(this.targets, target.id, target);
         });
       });
-    }, 3000);
+    });
+    this.polling = setInterval(() => this.updateTargetDetails(), 1000);
   }
 };
 </script>
-<style scoped></style>
+<style lang="stylus" scoped>
+@import "~vuetify/src/stylus/main";
+
+kbd:before, kbd:after
+  content ""
+
+kbd
+  @extends .pa-2
+</style>
