16cd8ce933511536d72331a0aa40f6ecfedffc48
Matthew de Verteuil <onceuponajooks@gmail.com>
2019-05-23 02:51:08 -0400

    Fix API base URL for heroku

--- mig3-ui/src/services/axios-client.js
+++ mig3-ui/src/services/axios-client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default () =>
   axios.create({
-    baseURL: process.env.NODE_ENV === "development" ? "http://localhost:8000/api/" : "",
+    baseURL: process.env.NODE_ENV === "development" ? "http://localhost:8000/api/" : "/api/",
     headers: {
       Accepts: "application/json",
       "Access-Control-Allow-Origin": "*",
