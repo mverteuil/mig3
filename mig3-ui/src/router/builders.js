848df0041976b889f001cf2cf837abab4fa875d3
Matthew de Verteuil <onceuponajooks@gmail.com>
2019-05-27 00:03:29 -0400

    Builders refactor

--- mig3-ui/src/router/builders.js
+++ mig3-ui/src/router/builders.js
@@ -1,5 +1,12 @@
+import store from "@/store/store";
+import { FETCH_BUILDERS } from "@/store/action-types";
+
 export default {
   path: "/builders/",
   name: "Builders",
-  component: () => import(/* webpackChunkName: "builders" */ "@/views/Builders.vue")
+  component: () => import(/* webpackChunkName: "builders" */ "@/views/Builders.vue"),
+  beforeEnter: async (to, from, next) => {
+    await store.dispatch(FETCH_BUILDERS);
+    next();
+  }
 };
